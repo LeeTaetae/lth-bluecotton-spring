db9e0546c3611af40ef56a8b8d6be6e04d866129
junseoch <ljs862200@gmail.com>
2025-11-06 11:14:21 +0900

    feat: 샵 메인 페이지 필터 상품 검색 로직 추가

--- src/main/java/com/app/bluecotton/service/ShopService.java
+++ src/main/java/com/app/bluecotton/service/ShopService.java
@@ -4,13 +4,11 @@ import com.app.bluecotton.domain.dto.ProductListResponseDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ShopService {
 
-    // 샵 메인 페이지 전체 조회
-    public List<ProductListResponseDTO> getProducts();
-
-    // 상품 키워드 검색 조회
-//    public ProductListResponseDTO getProductByProductName(String productName);
+    // 상품 조건 조회
+    public List<ProductListResponseDTO> getProductByFilter(Map<String,Object> params);
 
 }
